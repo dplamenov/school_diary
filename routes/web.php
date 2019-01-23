982550feaf72032c673d97eed800c2870055f663
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-01-23 15:30:25 +0200

    bug fix

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,6 @@ Route::get('director/addsubject', 'DirectorController@addSubjectForm');
 Route::post('director/addsubject', 'DirectorController@addSubject');
 
 Route::get('director/addclass', 'DirectorController@addClassForm');
-Route::post('director/addclass', 'DirectorController@addClassForm');
+Route::post('director/addclass', 'DirectorController@addClass');
 
 Route::get('/logout', 'DefaultController@logout');
