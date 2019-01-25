cc0028cad054362453bedbde1fa5f7c35f10aabd
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-01-25 11:42:52 +0200

    add security

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 
 class TeacherController
 {
-    public function listOfClass($id){
+    public function listOfClass(Request $request, $id)
+    {
+        if($request->session()->get('islogged') !== true or $request->session()->get('user_data')['type'] != 'Teacher'){
+            return redirect()->route('home');
+        }
+        var_dump($request->session());
         return $id;
     }
 }
