e0f00d0b3291ba59e30c553a0aa26fe3150125ff
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-01-25 12:14:23 +0200

    bug fix

--- resources/views/teacher.blade.php
+++ resources/views/teacher.blade.php
@@ -6,13 +6,14 @@
     <div>
         @if(count($class) > 0 or count($classes) >0)
             <p>You teach the following classes:</p>
+            @php($_class = '')
             @foreach($class as $item)
                 @php($_class  = $item->class_name)
                 <p><a href="{{url('class/' . $item->class_id)}}">{{$item->class_name}}</a> - Your class</p>
             @endforeach
             @foreach($classes as $class)
                 @if($_class != $class->class_name)
-                    <p><a href="{{url('class/' . $item->class_id)}}">{{$class->class_name}}</a></p>
+                    <p><a href="{{url('class/' . $class->class_id)}}">{{$class->class_name}}</a></p>
                 @endif
             @endforeach
         @else
