f09c1d3109d1fabb7590d77d5410d3e4c83fdd57
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-01-29 17:47:45 +0200

    add test for post

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,14 +2,18 @@
 
 namespace Tests;
 
+use App\Http\Controllers\DefaultController;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
-
+    private $controller;
     public function testHomeRoute()
     {
         $this->get('/')->assertStatus(200);
+    }
+    public function testHomeRouteByPost(){
+        $this->post('/')->assertStatus(500);
     }
 }
