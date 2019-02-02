1b56d75c63f6e3b63931152a4fd5399869cd193f
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-02-02 14:47:45 +0200

    bug fix

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -10,7 +10,7 @@ class TeacherController
 {
     public function listOfClass(Request $request, $class_id)
     {
-        $teacher_id = $request->session()->get('user_data')['id'];
+        $teacher_id = $request->session()->get('user_data')['tid'];
         if ($request->session()->get('islogged') !== true or $request->session()->get('user_data')['type'] != 'Teacher') {
             return redirect()->route('home');
         }
@@ -20,12 +20,12 @@ class TeacherController
         }
 
         $subjects = [];
+        echo $teacher_id;
         $result = DB::select('SELECT * FROM `teacher_classes` WHERE `teacher_id` = ? and class_id = ?', [$teacher_id, $class_id]);
         for ($i = 0; $i <= count($result) - 1; $i++) {
             $subject = DB::select('SELECT * FROM `subjects` WHERE subject_id = ?', [$result[$i]->subject_id]);
             $subjects[] = $subject[0]->subject_name;
         }
-        var_dump($subjects);
         return view('teacher_class', ['title' => 'Class ID :' . $class_id, 'subjects' => $subjects]);
 
     }
