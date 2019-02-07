522e9589a6c26a8bae7ffadaafcee98c46c58049
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-02-07 16:15:47 +0200

    bug fix

--- resources/views/addteacherform.blade.php
+++ resources/views/addteacherform.blade.php
@@ -7,22 +7,28 @@
             <p>{{$error}}</p>
         @endforeach
     @endif
-    <form action="{{url('director/addteacher')}}" method="post">
-        @method('post')
-        @csrf
-        <label>Full name<input type="text" name="fullname"/></label>
-        <br><label>
-            Subjects
-            <select multiple="multiple" name="subjects[]">
-                @foreach($subjects as $key => $subject)
-                    <option value="{{$key}}">{{$subject}}</option>
-                @endforeach
-            </select>
-        </label>
-        <label>
-            Email<input type="text" name="email"/>
-        </label>
-        <input type="submit"/>
-    </form>
+    @if(count($subject) > 0)
+        <form action="{{url('director/addteacher')}}" method="post">
+            @method('post')
+            @csrf
+            <label>Full name<input type="text" name="fullname"/></label>
+            <br><label>
+                Subjects
+                <select multiple="multiple" name="subjects[]">
+                    @foreach($subjects as $key => $subject)
+                        <option value="{{$key}}">{{$subject}}</option>
+                    @endforeach
+                </select>
+            </label>
+            <label>
+                Email<input type="text" name="email"/>
+            </label>
+            <input type="submit"/>
+        </form>
+
+    @else
+        <p>First add subject from <a href="{{url('director/addsubject')}}">here</a>.</p>
+    @endif
+
 @endsection
 
