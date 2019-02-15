777d05391782e5007487b917c9e2446a02eeb08e
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-02-15 11:19:16 +0200

    StudentController

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -2,10 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Models\Students;
+use Illuminate\Http\Request;
 
 class StudentController
 {
-    public function studentFromTeacher(){
+    public function studentFromTeacher(Request $request, $student_id)
+    {
+        if ($request->session()->get('islogged') !== true or $request->session()->get('user_data')['type'] != 'Teacher') {
+            return redirect()->route('home');
+        }
         return view('student_teacher');
     }
 }
