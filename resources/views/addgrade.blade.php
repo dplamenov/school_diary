b127ccff80b07f1007d74adc02ee5131af33772e
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-02-18 16:11:09 +0200

    add grade view

--- resources/views/addgrade.blade.php
+++ resources/views/addgrade.blade.php
@@ -8,5 +8,11 @@
     <form action="{{url('student/add/grade')}}" method="post">
         @csrf
         @method('post')
+        <select>
+            @foreach($grades as $grade)
+                <option value="{{$grade->grade_id}}">{{$grade->grade_name}}</option>
+            @endforeach
+        </select>
+
     </form>
 @endsection
