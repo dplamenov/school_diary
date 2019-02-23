59fe0522f549195d391eb927cd62d189f001523b
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-02-23 11:59:57 +0200

    director view

--- resources/views/classinfo.blade.php
+++ resources/views/classinfo.blade.php
@@ -4,8 +4,12 @@
     <a href="{{url('/')}}">Go back</a>
     <p>Class: {{$class_name}}</p>
     <p>Students:</p>
-    @foreach($students as $student)
-        <p>{{$student->student_name}}</p>
-        <p>{{number_format($student->average_grade, 2)}}</p>
-    @endforeach
+    <table>
+        @foreach($students as $student)
+            <tr>
+                <td>{{$student->student_name}}</td>
+                <td>{{number_format($student->average_grade, 2)}}</td>
+            </tr>
+        @endforeach
+    </table>
 @endsection
