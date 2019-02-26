b5daf200b6cb3d8d87033b933cf36c39092bf50e
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-02-26 15:19:27 +0200

    add student form

--- resources/views/addstudent.blade.php
+++ resources/views/addstudent.blade.php
@@ -2,6 +2,8 @@
 @section('title', 'Add student')
 @section('container')
     <form method="post" action="{{url('director/add/student')}}">
+        @csrf
+        @method('post')
         <input type="hidden" name="class_id" value="{{$class_id}}"/>
         <label>
             Student name
