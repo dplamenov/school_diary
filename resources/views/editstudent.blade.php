eeab3e5b7e3935fa897b197ed16f298b8a657d8c
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-02-26 18:54:13 +0200

    display errors under the form

--- resources/views/editstudent.blade.php
+++ resources/views/editstudent.blade.php
@@ -2,6 +2,12 @@
 @section('title', 'Edit student')
 @section('container')
     <p>You`re edit student {{$student->student_name}}.</p>
+    @if ($errors->any())
+        @foreach($errors->all() as $error)
+            <p>{{$error}}</p>
+        @endforeach
+
+    @endif
     <form method="post" action="{{url('director/edit/student')}}">
         @csrf
         @method('post')
