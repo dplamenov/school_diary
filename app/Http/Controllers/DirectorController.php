0d07466b34ae73e07b534f9fe1bae21b1a0b35f7
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-02-27 18:08:27 +0200

    edit subject view

--- app/Http/Controllers/DirectorController.php
+++ app/Http/Controllers/DirectorController.php
@@ -352,6 +352,7 @@ class DirectorController extends Controller
         if ($request->session()->get('user_data')['type'] != 'director') {
             return redirect()->route('home');
         }
+        return view('editsubject');
 
     }
 }
---
+++ resources/views/editsubject.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.layout')
+@section('title', 'Edit subject')
+@section('container')
+
+
+@endsection
