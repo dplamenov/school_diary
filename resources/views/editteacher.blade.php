8d616e4618c3d2ed7177d6e7f3ba463f0a4410de
Dimitar Plamenov <dimitar.plamenov@gmail.com>
2019-02-28 14:55:41 +0200

    edit teacher form

--- resources/views/editteacher.blade.php
+++ resources/views/editteacher.blade.php
@@ -4,4 +4,12 @@
 @endsection
 @section('container')
     <p>You will edit {{$teacher->teacher_name}} teacher.</p>
+    <form method="post" action="{{url('teacher/edit')}}">
+        @csrf
+        @method('post')
+        <label>
+            Teacher name
+            <input type="text" name="name" value="{{$teacher->teacher_name}}"/>
+        </label>
+    </form>
 @endsection
